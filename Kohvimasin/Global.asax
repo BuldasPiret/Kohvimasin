5724b989bd830cdd4b1fbb1ada2adcc8fa9d1b82
opilane <opilane@HP-CZC6297B86>
2020-01-30 15:45:35 +0200

    Add project files.

---
+++ Kohvimasin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kohvimasin.MvcApplication" Language="C#" %>
---
+++ Kohvimasin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kohvimasin.Startup))]
+namespace Kohvimasin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
